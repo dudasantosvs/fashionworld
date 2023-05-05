4b933867b75f468dffcb2ac84ba2e8de81612b75
dudasantosvs <111027198+dudasantosvs@users.noreply.github.com>
2023-05-05 08:54:38 -0300

    Delete source directory

--- source/Boot/Config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-const CONF_DB_HOST = "localhost";
-const CONF_DB_USER = "root";
-const CONF_DB_PASS = "12345";
-const CONF_DB_NAME = "db_3am"; // aqui deve ser alterado para o nome do banco de dados
-
-const CONF_URL_BASE = "localhost/fashionworld"; // para produção
-const CONF_URL_TEST = "localhost/fashionworld"; //  para teste
